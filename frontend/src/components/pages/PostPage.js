15cbd3cd69014cebd1f7f61eda7d93df98365a9e
FelBenini <felbenini@gmail.com>
2023-03-06 20:48:31 -0300

    Added basic post page and content display functionality

--- frontend/src/components/pages/PostPage.js
+++ frontend/src/components/pages/PostPage.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import parse from 'html-react-parser'
+import { useParams } from 'react-router-dom'
 
 const PostPage = () => {
+  const [content, setContent] = useState({})
+  const {id} = useParams()
+  const fetchContent = async (id) => {
+    const {data} = await axios.get(`http://localhost:4000/postcontent/${id}`)
+    setContent(data)
+  }
+  useEffect(() => {
+    fetchContent(id)
+  })
   return (
-    <div>PostPage</div>
+    <div>
+      <h1>{content.title}</h1>
+      <h3>{parse(`${content.summary}`)}</h3>
+      {parse(`${content.content}`)}
+    </div>
   )
 }
 
--- frontend/src/components/postsCard.js
+++ frontend/src/components/postsCard.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const PostsCard = ({info, index}) => {
   return (
-    <Link to={`/post/${info.title}/${info._id}`} style={{background: `linear-gradient(to top, rgba(0, 0, 0, 0.741), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(http://localhost:4000/${info.img})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center'}} key={index} className='postCard'>
+    <Link to={`/post/${info.title}/${info._id}`} style={{background: `linear-gradient(to top, rgba(0, 0, 0, 0.741), rgba(0, 0, 0, 0.341), rgba(0, 0, 0, 0)), url(http://localhost:4000/${info.img})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center'}} key={index} className='postCard'>
         <h2>{info.title}</h2>
         <p>{info.summary}</p>
     </Link>
