17a576f84eba460845a778f562001107b3b77c46
FelBenini <felbenini@gmail.com>
2023-03-07 20:53:35 -0300

    Added pagination to the homepage frontend

--- frontend/src/components/pages/HomePage.js
+++ frontend/src/components/pages/HomePage.js
@@ -1,21 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import PostsCard from '../postsCard'
+import { useSearchParams, useLocation, useNavigate } from 'react-router-dom'
+import { Pagination } from '@mui/material'
 
 const HomePage = () => {
   const [posts, setPosts] = useState([])
+  const navigate = useNavigate()
+  const {search} = useLocation()
+  const [searchParams] = useSearchParams();
+  const [page, setPage] = useState(searchParams.get('page'))
+  const [pageCount, setPageCount] = useState()
+  console.log(page)
   const [firstPosts, setFirstPosts] = useState([])
-  const fetchPosts = () => {
-    axios.get('http://localhost:4000/posts').then(response => {
+
+  const fetchPosts = (page) => {
+    axios.get(`http://localhost:4000/posts?page=${page || 1}`).then(response => {
       setPosts(response.data.posts)
       setFirstPosts(response.data.posts.slice(0, 9))
+      setPageCount(response.data.pagesNumber)
   })}
   useEffect(() => {
-    fetchPosts() // eslint-disable-next-line
-  },[])
+    setPage(searchParams.get('page') || 1)
+    fetchPosts(page)
+  },[page, search])
   const postsMap = firstPosts.map((post, i) => {
     return <PostsCard key={i} index={i} info={post}/>
   })
+
+  const handlePageChange = (event, value) => {
+    
+    navigate({
+      pathname: '/',
+      search: `?page=${value}`,
+    });
+  }
   return (
     <section id='homePage'>
       <h1>Latest posts</h1>
@@ -29,6 +48,8 @@ const HomePage = () => {
           )
         })}
       </div>
+      <Pagination page={parseInt(page)} sx={{width: '100%', display: 'flex', justifyContent: 'center'}} count={pageCount} onChange={handlePageChange}
+      variant="outlined" shape="rounded" />
     </section>
   )
 }
--- frontend/src/components/postsCard.js
+++ frontend/src/components/postsCard.js
@@ -2,8 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const PostsCard = ({info, index}) => {
+  function getStyle() {
+    let style = {
+                backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.541), rgba(0, 0, 0, 0.141), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(http://localhost:4000/${info.img})`,
+                backgroundSize: "cover",
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center center'
+            }
+    return style;
+}
   return (
-    <Link to={`/post/${info.title}/${info._id}`} style={{background: `linear-gradient(to top, rgba(0, 0, 0, 0.541), rgba(0, 0, 0, 0.141), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(http://localhost:4000/${info.img})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center'}} key={index} className='postCard'>
+    <Link to={`/post/${info.title}/${info._id}`} style={getStyle()} key={index} className='postCard'>
         <h2>{info.title}</h2>
         <p>{info.summary}</p>
     </Link>
